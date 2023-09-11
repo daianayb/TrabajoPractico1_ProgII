4d99bfd974f8293d9363e6f7d5c9b7f998756467
daianayb <96448936+daianayb@users.noreply.github.com>
2023-09-10 22:39:27 -0300

    Add files via upload

---
+++ TP1_ej3.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp1_ej3;
+
+/**
+ *
+ * @author daian
+ */
+public class TP1_ej3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new ej3().setVisible(true);
+            }
+        });
+    }
+    
+}
---
+++ TP1_ej4.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp1_ej4;
+
+/**
+ *
+ * @author daian
+ */
+public class TP1_ej4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new ej4().setVisible(true);
+            }
+        });
+    }
+    
+}
